b1886b390aa28961761b6f61bf3555d35ddc59ca
Emil <eculic17@gmail.com>
2013-11-26 01:33:31 +0000

    Removed extra team members, commented out comments in twitter js

--- assets/js/application-blog-list.js
+++ assets/js/application-blog-list.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
-    
+ /*     
     *   PAGE | Twitter 
     *   
     *   CONFIGURE FIRST
     *
     *   Pull latest tweets from user.
-    *   Configuration: /plugins/twitter/index.php
+    *   Configuration: /plugins/twitter/index.php*/
     
     $('.twitterfeed').tweet({
         modpath: '../../plugins/twitter/',
